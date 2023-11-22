8e2700be5dc23ee40e36468a008033ae977c5411
Antonio Gómez <antonio.gomez21@alu.uclm.es>
2023-11-22 19:21:32 +0100

    Fix mean in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from mapa import Mapa
 import os.path
+import json
+import numpy as np
+
+with open('config.json', 'r') as file:     config = json.load(file)
 
 def test(filename : str, mapa : "Mapa", roundval = 3, do_round = False) -> None:
     count = 0
@@ -47,10 +51,8 @@ def main():
     count, count_error, lenlines = test(os.path.join(DATAFOLDER, "test_map_original.txt"), map, roundval, do_round)
     print(f"Out of {lenlines}, {count}({count/lenlines * 100:.2f}%) fails, {count_error}({count_error/count * 100:.2f}% error/fail, {count_error/lenlines * 100:.2f}% error/total) with distance > {10 ** - roundval}" if count != 0 else "No errors")
 
-
-    mean = lambda x: round(sum(x) / len(x), 3) # redondea la media a 3 decimales
     # leer si se existe else crear
-    resized = Mapa(os.path.join(DATAFOLDER, "300_mean.hdf5")) if os.path.isfile(os.path.join(DATAFOLDER, "300_mean.hdf5")) else map.resize(300, mean, os.path.join(DATAFOLDER, "300_mean.hdf5"))
+    resized = Mapa(os.path.join(DATAFOLDER, "300_mean.hdf5")) if os.path.isfile(os.path.join(DATAFOLDER, "300_mean.hdf5")) else map.resize(300, np.mean, os.path.join(DATAFOLDER, "300_mean.hdf5"))
     print(f"testing map_300_mean")
     count, count_error, lenlines = test(os.path.join(DATAFOLDER, "test_map_300_mean.txt"), resized, roundval, do_round)
     print(f"Out of {lenlines}, {count}({count/lenlines * 100:.2f}%) fails, {count_error}({count_error/count * 100:.2f}% error/fail, {count_error/lenlines * 100:.2f}% error/total) with distance > {10 ** - roundval}" if count != 0 else "No errors")
