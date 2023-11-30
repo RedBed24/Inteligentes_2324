95d0fd86415d68a13d681ca4734c3ad13a9f7ad7
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-11-30 09:43:43 +0100

    Fix algoritmo usa else if en vez de elif

--- src/algoritmo_busqueda.py
+++ src/algoritmo_busqueda.py
@@ -15,15 +15,13 @@ def algoritmo_busqueda(problema : Problema, estrategia : "function", profundidad
         nodo=frontera.getNode()
         if problema.objetivo(nodo.estado):
             solucion = True
-        else:
-            if nodo.estado not in visitados and nodo.profundidad <= cota: 
-                visitados.append(nodo.estado)  
-                for sucesor in nodo.estado.sucessor():
-                    id_nodo += 1
-                    nuevo_nodo = Nodo(id_nodo, nodo, sucesor.to_state, nodo.profundidad + 1, nodo.costo_distancia + sucesor.length, max(nodo.costo_max_desnivel, sucesor.heigth), problema.heuristica(sucesor.to_state), sucesor)
-                    nuevo_nodo.valor = estrategia(nuevo_nodo)
-                    frontera.add(nuevo_nodo)
-
+        elif nodo.estado not in visitados and nodo.profundidad <= profundidad_maxima:
+            visitados.append(nodo.estado)  
+            for sucesor in nodo.estado.sucessor():
+                id_nodo += 1
+                nuevo_nodo = Nodo(id_nodo, nodo, sucesor.to_state, nodo.profundidad + 1, nodo.costo_distancia + sucesor.length, max(nodo.costo_max_desnivel, sucesor.heigth), problema.heuristica(sucesor.to_state), sucesor)
+                nuevo_nodo.valor = estrategia(nuevo_nodo)
+                frontera.add(nuevo_nodo)
 
     camino = []
     if solucion:
