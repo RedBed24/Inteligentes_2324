d870c5b922655700c17dc34622c6cdf944a45f4c
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-12-10 13:34:22 +0100

    Main que hace todo automáticamente

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import setup
+import test_tarea1
+import test_tarea2
+import test_tarea3
+
+def main():
+    setup.main() # create all maps
+    test_tarea1.main()
+    test_tarea2.main()
+    test_tarea3.main()
+
+if __name__ == "__main__":
+    main()
