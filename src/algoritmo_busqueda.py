04592f7c74b620218c89958baf7a362fa7e04252
Antonio Gómez <antonio.gomez21@alu.uclm.es>
2024-01-22 16:27:00 +0100

    Examen Ordinario

--- src/algoritmo_busqueda.py
+++ src/algoritmo_busqueda.py
@@ -2,6 +2,7 @@ from typing import Callable
 from nodo import Nodo
 from frontera import Frontera
 from problema import Problema
+from espacio_estados import Accion
 
 def algoritmo_busqueda(problema : Problema, estrategia : Callable[[Nodo], float], heuristica : Callable[[Nodo, Problema], float], profundidad_maxima : int) -> list[Nodo]:
     id_nodo = 0
@@ -12,9 +13,16 @@ def algoritmo_busqueda(problema : Problema, estrategia : Callable[[Nodo], float]
     nodo.heuristica = heuristica(nodo, problema)
     nodo.valor = estrategia(nodo)
     frontera.add(nodo)
-
+    nodos_este_expandidos=0 # AÑADE PARA RESOLUCIÓN EXAMEN ORDINARIO
+    nodos_este_sacados_frontera=0 # AÑADE PARA RESOLUCIÓN EXAMEN ORDINARIO
     while len(frontera) and not solucion:
         nodo = frontera.getNode()
+        if nodo.accion is not None and nodo.accion.direction == Accion.EAST:
+            nodos_este_sacados_frontera = nodos_este_sacados_frontera+1 # AÑADE PARA RESOLUCIÓN EXAMEN ORDINARIO
+        #if nodo.estado.p.y == 3108281 and nodo.estado.p.x == 281933 : #El id del último nodo que visito el estado (3108281, 281933) es 
+        #    print(nodo.id)
+        # if nodo.estado.parent.parent is not None and nodo.estado.parent.parent.p.y == 3108281 and nodo.estado.parent.parent.p.x == 281933 : #El id del nodo cuyo abuelo visito el estado (3108281, 281933)
+        #    print(nodo.id)
         if problema.objetivo(nodo.estado):
             solucion = True
         elif nodo.estado not in visitados and nodo.profundidad < profundidad_maxima:
@@ -25,7 +33,10 @@ def algoritmo_busqueda(problema : Problema, estrategia : Callable[[Nodo], float]
                 nuevo_nodo.heuristica = heuristica(nuevo_nodo, problema)
                 nuevo_nodo.valor = estrategia(nuevo_nodo)
                 frontera.add(nuevo_nodo)
+                if(sucesor.direction==Accion.EAST):
+                    nodos_este_expandidos=nodos_este_expandidos+1 # AÑADE PARA RESOLUCIÓN EXAMEN ORDINARIO
 
+    print(f"nodos expandidos: {nodos_este_expandidos}, nodos sacados: {nodos_este_sacados_frontera}, nodos que se han quedado en la frontera: {nodos_este_expandidos-nodos_este_sacados_frontera}") # AÑADE PARA RESOLUCIÓN EXAMEN ORDINARIO
     camino = []
     if solucion:
        camino = nodo.funcion_camino()
--- src/setup.py
+++ src/setup.py
@@ -1,26 +1,45 @@
-from mapa import Mapa
-
-import numpy as np
 import os.path
 import json
 
-def set_up_maps(map, config):
-    factor = 300
-    transform = np.mean
-    map_name = config["maps_names"]["300_mean"]
-
-    map.resize(factor, transform, os.path.join(config["data_folder"], map_name))
-
-    factor = 400
-    transform = np.max
-    map_name = config["maps_names"]["400_max"]
-
-    map.resize(factor, transform, os.path.join(config["data_folder"], map_name))
+import numpy as np
+from problema import Problema
+from mapa import Mapa
+import estrategias
+import heuristicas
+from algoritmo_busqueda import algoritmo_busqueda
 
 def main():
     with open('config.json', 'r') as file: config = json.load(file)
+    os.makedirs("resultados", exist_ok=True)
 
-    set_up_maps(Mapa(os.path.join(config["data_folder"], config["maps_names"]["original"])), config)
+    factor = 135
+    transform = np.mean
+    map_name = os.path.join(config["data_folder"], "Gomerazoom135.hdf5")
+
+    mp = Mapa(os.path.join(config["data_folder"], config["maps_names"]["original"]))
+    if not os.path.isfile(map_name):
+        new = mp.resize(factor, transform, map_name)
+
+    y0, x0 = 3115231, 270163
+    yf, xf = 3110101, 292843
+
+    maxdepth = 999999
+
+    resultado = algoritmo_busqueda(
+        Problema(map_name, y0, x0, yf, xf),
+        estrategias.ASTAR,
+        heuristicas.euclidean, 
+        maxdepth 
+    )
+
+    with open(os.path.join("resultados", "solucion.txt"), "w") as f:
+        f.write(f"file:solucion.txt\n")
+        f.write(f"init:({y0}, {x0})\n")
+        f.write(f"goal:({yf}, {xf})\n")
+        f.write(f"strategy:ASTAR\n") 
+        f.write(f"maxdepth:{maxdepth}\n")
+        for nodo in resultado:
+            f.write(f"{nodo}\n")
 
 if __name__ == "__main__":
     main()
